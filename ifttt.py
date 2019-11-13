bcc67796f1d6f2c5ecfe8413859e40dbf4a62bcb
ztmills <hacker.6284@gmail.com>
2019-11-12 22:38:06 -0700

    v3 finished?

--- ifttt.py
+++ ifttt.py
@@ -1,5 +1,14 @@
+#!/usr/bin/env python
+# Function which makes IFTTT requests for the webhooks applet
+
 import requests
 
+
 def ifttt(name, value1, value2, value3, key):
-    payload = { 'value1' : value1, 'value2' : value2, 'value3' : value3}
-    requests.post("https://maker.ifttt.com/trigger/%s/with/key/%s"%(name, key), data=payload)
+    """Make an ifttt webhooks call on hook *name* with value payloads.
+    Pass secret key as last argument
+    """
+
+    payload = {'value1': value1, 'value2': value2, 'value3': value3}
+    requests.post("https://maker.ifttt.com/trigger/{}/with/key/{}".format(
+        name, key), data=payload)
